684d5dc591409cff6a8b99a910b38713a9b12fbc
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-04-07 20:16:36 +0530

    commit

--- src/Components/Parts/Main.js
+++ src/Components/Parts/Main.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Dashboard from '../Dashboard/Dashboard';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import ProjectDetails from '../Projects/ProjectDetails';
 
 
 
-const Main = () => {
+class Main extends Component  {
+    render(){
     return(
         <BrowserRouter>
         <Switch>
 
         <div className="main">
-        <Route path='/'component={Dashboard} />
+        <Route exact  path='/' component={Dashboard} />
+
+        <Route path='/project/:post_id' component = {ProjectDetails}/>
           
         </div>
         </Switch>
         </BrowserRouter>
 
     )
+} 
 }
 
 export default Main;
---
+++ src/Components/Projects/ProjectDetails.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProjectDetails=(props) =>{
+    let id = props.match.params.post_id;
+    console.log(props);
+    
+    return (
+    
+       <div className="container section project-details">
+           <div className="card">
+               <div className="card-content">
+                   <span className="card-title">Project tittle - {id}</span>
+                   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit magni beatae voluptatem libero quaerat rerum id voluptate aliquam dolores aperiam. Autem unde ullam in earum distinctio molestiae recusandae corrupti modi?</p>
+               </div>
+               <div className="card-action grey lighten-3 grey-text">
+                   <p>Posted by Ashish Maurya</p>
+                   <p>todays date</p>
+               </div>
+           </div>
+       </div>
+    )
+}
+
+export default ProjectDetails
