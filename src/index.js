d2c4d7f67a8a32b58bf056ce987e3347133d6a30
Ashish maurya <50199029+Ashumaurya@users.noreply.github.com>
2020-04-08 22:47:00 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux'
+import RootReducer from './store/reducers/RootReducer'
+import {Provider} from 'react-redux'
 
-const store = createStore();
+const store = createStore(RootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store = {store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducers/ProjectReducer.js
@@ -0,0 +1,9 @@
+const initState  ={
+
+}
+
+const ProjectReducer = (state = initState  , action) => {
+    return state; 
+};
+
+export default ProjectReducer ;
